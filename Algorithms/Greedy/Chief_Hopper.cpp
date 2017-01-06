ae437e4af762d26a3dd099f8fba838e01adefd14
Tsuda Kageyu <tsuda.kageyu@gmail.com>
2017-01-06 17:20:42 +0900

    Update "Chief Hopper" to be simpler.

--- Algorithms/Greedy/Chief_Hopper.cpp
+++ Algorithms/Greedy/Chief_Hopper.cpp
@@ -14,12 +14,8 @@ int main() {
         cin >> x;
     
     int e = 0;
-    for (auto it = h.crbegin(); it != h.crend(); ++it) {
-        if (*it > e)
-            e = e + (*it - e + 1) / 2;
-        else
-            e = e + (*it - e) / 2;
-    }
+    for (auto it = h.crbegin(); it != h.crend(); ++it)
+        e = (*it + e + 1) / 2;
     
     cout << e << endl;
     
